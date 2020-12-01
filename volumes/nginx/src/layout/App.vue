e9c503a1a86e6e626769121c492de98cadbcece9
ando <ando@jimbodenki.co.jp>
2020-12-01 21:43:04 +0900

    Change locate folder

---
+++ volumes/nginx/src/layout/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="section">
+        <header>
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+            </ul>
+        </header>
+        <router-view />
+    </div>
+</template>
+
+<style scoped lang="scss">
+header {
+    background: #777;
+    ul {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        list-style: none;
+        li {
+            margin: 10px;
+            a {
+                color: #fff;
+            }
+        }
+    }
+}
+</style>
